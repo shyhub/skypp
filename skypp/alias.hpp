7eafb68bd229b6e912b98c349dc5e310af655a4c
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-03-27 16:24:13 +0000

    Create aliases module with skypp defined aliases and user defined aliases (skypp/alias.hpp)

---
+++ skypp/alias.hpp
@@ -0,0 +1,16 @@
+#ifndef __SKYPP_ALIAS_HPP__
+#define __SKYPP_ALIAS_HPP__
+
+/* skypp defined aliases */
+#define type      using
+#define defstruct typedef struct
+#define defunion  typedef union
+#define defenum   typedef enum
+
+#define print printf
+
+
+/* user defined aliases */
+
+
+#endif /* __SKYPP_ALIAS_HPP__ */
