96ad0bdd10f008c86268911dc7d030cf63889809
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-03-27 16:16:38 +0000

    Add byte, iX, uX and fX data types to types module (skypp/types.hpp)

--- skypp/types.hpp
+++ skypp/types.hpp
@@ -4,6 +4,24 @@
 typedef void* let;
 typedef void* T;
 
+typedef char byte;
+
+typedef char      i8;
+typedef short     i16;
+typedef int       i32;
+typedef long      i64;
+typedef long long i128;
+
+typedef unsigned char      u8;
+typedef unsigned short     u16;
+typedef unsigned int       u32;
+typedef unsigned long      u64;
+typedef unsigned long long u128;
+
+typedef float       f32;
+typedef double      f64;
+typedef long double f128;
+
 typedef void none;
 
 
